cfbcaab63ff09db582b145fe2b702f795b6a3241
Devanshu Deshav <deshav.devanshu2017487@gmail.com>
2024-02-07 18:17:39 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,35 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { dbConnection } from "./database/dbConnection.js";
+1;
+import { errorMiddleware } from "./error/error.js";
+import reservationRouter from "./routes/reservationRoute.js";
+
+const app = express();
+dotenv.config({ path: "./config/config.env" });
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["POST", "GET", "OPTIONS"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/v1/reservation", reservationRouter);
+
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    success: true,
+    message: "HELLO WORLD",
+  });
+});
+
+dbConnection();
+
+app.use(errorMiddleware);
+
+export default app;
